574f33346f9137e0f565deb7f4932d1be6e97ff1
Jason Wang <wyqdgggfk@aol.com>
2020-01-17 16:04:35 +0800

    what is this
    what does this mean

--- 1csv_read_with_simple_parsing_and_write.py
+++ 1csv_read_with_simple_parsing_and_write.py
@@ -1,4 +1,5 @@
 import sys
+
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
@@ -14,3 +15,4 @@ with open(input_file,'r',newline='') as filereader:
 			row_list = row.split(',')
 			print(row_list)
 			filewriter.write(','.join(map(str, row_list))+'\n')
+
