325f5989d1ecb03f8e2c1071c6038cd2e56f1566
Jason Wang <wyqdgggfk@aol.com>
2020-01-23 15:42:08 +0800

    update wrong words

--- 第2章所需资料/DEvideo_reader_column_by_index.py
+++ 第2章所需资料/DEvideo_reader_column_by_index.py
@@ -4,7 +4,7 @@ import time
 
 pattern = re.compile('T[\d]+:[\d]+:[\S]+') # 正则表达式，用来筛选 publish_time 中的日期
 
-"""实在不行再写好代码后，跑去终端运行，就直接把路径写下来吧"""
+"""实在不想在写好代码后，跑去终端运行，就直接把路径写下来吧"""
 input_file = '/Users/jason/Documents/GitHub/NoteforPythonDataAnalyze/第2章所需资料/Trending_YouTube_Video_Statistics/DEvideos.csv'
 output_file = '/Users/jason/Documents/GitHub/NoteforPythonDataAnalyze/第2章所需资料/DEvideo_write_column_by_index.csv'
 with open(input_file,'r',newline='') as csv_in_file:
