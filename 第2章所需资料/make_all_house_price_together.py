9093c645a8b1a05009f38f605e7b82db4f9a7eb3
Jason Wang <wyqdgggfk@aol.com>
2020-01-31 00:10:52 +0800

    update the code and note

---
+++ 第2章所需资料/make_all_house_price_together.py
@@ -0,0 +1,28 @@
+import csv
+import glob
+import os
+import sys
+
+input_path = sys.argv[1]
+output_file = sys.argv[2]
+first_file = True
+
+for input_file in glob.glob(os.path.join(input_path, '*csv')):
+	message = "Dealing with file " + str(os.path.basename(input_file))
+#	print(message)
+	with open(input_file,'r',newline='') as csv_in_file:
+		with open(output_file,'a',newline='') as csv_out_file:
+			filereader = csv.reader(csv_in_file)
+			filewriter = csv.writer(csv_out_file)
+			row_counter = 1
+			if first_file:
+				for row in filereader:
+					if row_counter > 3 and row_counter < 9:
+						print(row)
+					row_counter += 1
+				first_file = False
+			else:
+				for row in filereader:
+					if row_counter > 4 and row_counter < 9:
+						print(row)
+					row_counter += 1
