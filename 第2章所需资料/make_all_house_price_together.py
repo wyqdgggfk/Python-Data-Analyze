0ee781c680719cda2d42c576ae7d78b308d55983
Jason Wang <wyqdgggfk@aol.com>
2020-01-31 00:30:30 +0800

    update code note and note

--- 第2章所需资料/make_all_house_price_together.py
+++ 第2章所需资料/make_all_house_price_together.py
@@ -18,9 +18,9 @@ for input_file in glob.glob(os.path.join(input_path, '*csv')):
 			row_counter = 1
 			if first_file:
 				for row in filereader:
-					if row_counter > 3 and row_counter < 9:
+					if row_counter > 3 and row_counter < 9: # 对于第一个处理的文件，前 3 行和第 9 行数据是不需要的，只保留从第 4 行开始的数据
 						if row_counter == 4:
-							row.insert(0,"地区")
+							row.insert(0,"地区") # 在第 4 行的开头插入一个地区列，用来区分这些数据是哪个省市的
 						else:
 							pattern = re.compile("\.csv")
 							location_name = re.sub(pattern, '', os.path.basename(input_file))
