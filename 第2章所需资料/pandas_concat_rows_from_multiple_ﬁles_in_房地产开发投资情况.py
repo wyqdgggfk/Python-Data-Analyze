1485a3ec32ca46c88e4b601baa2015605ce63bb4
Jason Wang <wyqdgggfk@aol.com>
2020-01-31 21:28:28 +0800

    Update pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py

--- 第2章所需资料/pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py
+++ 第2章所需资料/pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py
@@ -13,15 +13,18 @@ first_file = True
 for house_price_file in all_house_price_files:
 	if first_file:
 		"""为了知道汇总后的数据都是哪个省市的房地产数据，需要提前插入一列地区"""
+		district_names = []
 		file_name = os.path.basename(house_price_file)
 		pattern = re.compile('\.csv')
-		dictrict_name = re.sub(pattern, '', file_name)
+		district_name = re.sub(pattern, '', file_name)		
 
 		data_frame = pd.read_csv(house_price_file,header=None)
 		data_frame = data_frame.drop([0,1,2,8])
-		row_counter = len(data_frame)
+		for row in range(data_frame.shape[0]):
+			district_names.append(district_name)
+		data_frame.insert(0,'地区',['河南','河南','河南','河南','河南'])
 		
-		print(row_counter)
+		print(data_frame)
 		first_file = False
 #	else:
 		
