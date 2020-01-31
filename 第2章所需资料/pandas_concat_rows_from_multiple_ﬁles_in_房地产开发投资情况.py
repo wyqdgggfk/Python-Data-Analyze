36941c7abc7c5489d41861a95b1b8925614dfdb8
Jason Wang <wyqdgggfk@aol.com>
2020-01-31 20:01:14 +0800

    Update pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py

--- 第2章所需资料/pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py
+++ 第2章所需资料/pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py
@@ -5,3 +5,9 @@ import sys
 input_path = sys.argv[1]
 output_file = sys.argv[2]
 all_house_price_files = glob.glob(os.path.join(input_path,'*csv'))
+all_data_frames = []
+first_file = True
+
+for house_price_file in all_house_price_files:
+	
+	data_frame = 
