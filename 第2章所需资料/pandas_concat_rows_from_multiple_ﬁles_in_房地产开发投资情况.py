92b71612b1bc98ebb40b3659ffcece17ecb0dbfe
Jason Wang <wyqdgggfk@aol.com>
2020-01-31 21:47:45 +0800

    Update pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py

--- 第2章所需资料/pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py
+++ 第2章所需资料/pandas_concat_rows_from_multiple_ﬁles_in_房地产开发投资情况.py
@@ -12,17 +12,19 @@ first_file = True
 
 for house_price_file in all_house_price_files:
 	if first_file:
-		"""为了知道汇总后的数据都是哪个省市的房地产数据，需要提前插入一列地区"""
+		"""以下代码是用来获取当前处理 csv 文件的文件名"""
 		district_names = []
 		file_name = os.path.basename(house_price_file)
 		pattern = re.compile('\.csv')
 		district_name = re.sub(pattern, '', file_name)		
-
+		
+		"""还记得之前的 drop 函数和 iloc 函数么，又用到他们来挑选指定行了"""
 		data_frame = pd.read_csv(house_price_file,header=None)
 		data_frame = data_frame.drop([0,1,2,8])
 		data_frame.columns = data_frame.iloc[0]
 		data_frame = data_frame.reindex(data_frame.index.drop(3))
 		
+		"""为了知道汇总后的数据都是哪个省市的房地产数据，需要提前插入一列地区"""
 		for row in range(data_frame.shape[0]):
 			district_names.append(district_name)
 		data_frame.insert(0,'地区',district_names)
