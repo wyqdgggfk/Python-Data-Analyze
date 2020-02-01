f56285387035309599237059b1fae50afc51ad54
Jason Wang <wyqdgggfk@aol.com>
2020-02-02 00:17:40 +0800

    Update open_FPA_FOD_20170508.py

--- 第 4 章所需资料/open_FPA_FOD_20170508.py
+++ 第 4 章所需资料/open_FPA_FOD_20170508.py
@@ -1,5 +1,5 @@
 import sqlite3
 
-con = sqlite3.connect('/Users/jason/Documents/GitHub/NoteforPythonDataAnalyze/第 4 章所需资料/FPA_FOD_20170508.sqlite')
+con = sqlite3.connect('/Users/jason/Desktop/FPA_FOD_20170508.sqlite')
 
 cursor = con.execute("SELECT * FROM spatial_ref_sys")
