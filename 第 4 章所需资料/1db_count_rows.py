2fc69ddf24308cdf250e717aedb87bdd77b650ef
Jason Wang <wyqdgggfk@aol.com>
2020-02-01 22:35:56 +0800

    Update 1db_count_rows.py

--- 第 4 章所需资料/1db_count_rows.py
+++ 第 4 章所需资料/1db_count_rows.py
@@ -1,1 +1,13 @@
 import sqlite3
+
+# 创建 SQLite3 内存数据库
+# 创建带有 4 个属性的 sales 表
+
+con = sqlite3.connect(':memory')
+query = """CREATE TABLE sales
+			(customer VARCHAR(20),
+			product VARCHAR(40),
+			amount FLOAT,
+			date DATE);"""
+con.execute(query)
+con.commit()
