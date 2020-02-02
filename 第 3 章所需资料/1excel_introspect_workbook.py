bf6e4b93ce86f4cf6085a5fa1a857a728b446f28
Jason Wang <wyqdgggfk@aol.com>
2020-02-02 18:05:35 +0800

    Update 1excel_introspect_workbook.py

--- 第 3 章所需资料/1excel_introspect_workbook.py
+++ 第 3 章所需资料/1excel_introspect_workbook.py
@@ -3,3 +3,5 @@ from xlrd import open_workbook
 input_file = sys.argv[1]
 workbook = open_workbook(input_file)
 print("Number of worksheets:",workbook.nsheets)
+for worksheet in workbook.sheets():
+	print("Worksheet name:",worksheet.name,"\tRows:",worksheet.nrows,"\tColumns:",worksheet.ncols)
