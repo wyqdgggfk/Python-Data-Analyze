a9e0bcafb691e7938aa8dd1cab8056166a3c7beb
Jason Wang <wyqdgggfk@aol.com>
2020-02-04 16:36:19 +0800

    Update 10csv_reader_sum_average_from_multiple_ﬁles.py

--- 第 2 章所需资料/10csv_reader_sum_average_from_multiple_ﬁles.py
+++ 第 2 章所需资料/10csv_reader_sum_average_from_multiple_ﬁles.py
@@ -6,3 +6,5 @@ input_path = sys.argv[1]
 output_file = sys.argv[2]
 output_header_list = ['file_name','total_sales','average_sales']
 csv_out_file = open(output_file,'a',newline='')
+filewriter = csv.writerow(csv_out_file)
+filewriter.writerow(output_header_list)
