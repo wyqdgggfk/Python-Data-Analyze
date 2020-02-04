56049973857610581c670d653c8794ff540f6c01
Jason Wang <wyqdgggfk@aol.com>
2020-02-04 18:00:26 +0800

    Update 10csv_reader_sum_average_from_multiple_ﬁles.py

--- 第 2 章所需资料/10csv_reader_sum_average_from_multiple_ﬁles.py
+++ 第 2 章所需资料/10csv_reader_sum_average_from_multiple_ﬁles.py
@@ -16,4 +16,12 @@ for input_file in glob.glob(os.path.join(input_path, 'sales_*')):
 		header = next(filereader)
 		total_sales = 0.0
 		number_of_sales = 0.0
-		
+		for row in filereader:
+			sale_amount = row[3]
+			total_sales += float(str(sale_amount).strip('$').replace(',',''))
+			number_of_sales += 1
+		average_sales = '{0:.2f}'.format(total_sales/number_of_sales)
+		output_list.append(total_sales)
+		output_list.append(average_sales)
+		filewriter.writerow(output_list)
+csv_out_file.close()
