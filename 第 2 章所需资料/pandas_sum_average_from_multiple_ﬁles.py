9f3bcb0d748dcc2da2a70f7009c3dff00cf454cb
Jason Wang <wyqdgggfk@aol.com>
2020-02-05 00:57:43 +0800

    Update pandas_sum_average_from_multiple_ﬁles.py

--- 第 2 章所需资料/pandas_sum_average_from_multiple_ﬁles.py
+++ 第 2 章所需资料/pandas_sum_average_from_multiple_ﬁles.py
@@ -10,3 +10,9 @@ for input_file in all_files:
 	data_frame = pd.read_csv(input_file,index_col=None)
 	total_cost = pd.DataFrame([float(str(value).strip('$').replace(',','')) for value in data_frame.loc[:,'Sale Amount']]).sum()
 	average_cost = pd.DataFrame([float(str(value).strip('$').replace(',','')) for value in data_frame.loc[:,'Sale Amount']]).mean()
+	data = {'file_name':os.path.basename(input_file),'total_sales':total_cost,'average_sales':average_cost}
+	all_data_frames.append(pd.DataFrame(data,columns=['file_name','total_sales','average_sales']))
+	data_frames_concat = pd.concat(all_data_frames,axis=0,ignore_index=True)
+	data_frames_concat.to_csv(output_file,index=False)
+	
+"""本代码与源代码不同，有一处 total_cost 在源代码中是没有的，源代码写的是 total_sales"""
