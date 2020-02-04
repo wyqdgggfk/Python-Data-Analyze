76e7443886e098f72497c3db86f2a6543b6536c9
Jason Wang <wyqdgggfk@aol.com>
2020-02-05 00:52:14 +0800

    Update pandas_sum_average_from_multiple_ﬁles.py

--- 第 2 章所需资料/pandas_sum_average_from_multiple_ﬁles.py
+++ 第 2 章所需资料/pandas_sum_average_from_multiple_ﬁles.py
@@ -4,3 +4,9 @@ import os
 import sys
 input_path = sys.argv[1]
 output_file = sys.argv[2]
+all_files = glob.glob(os.path.join(input_path, 'sales_*'))
+all_data_frames = []
+for input_file in all_files:
+	data_frame = pd.read_csv(input_file,index_col=None)
+	total_cost = pd.DataFrame([float(str(value).strip('$').replace(',','')) for value in data_frame.loc[:,'Sale Amount']]).sum()
+	average_cost = pd.DataFrame([float(str(value).strip('$').replace(',','')) for value in data_frame.loc[:,'Sale Amount']]).mean()
