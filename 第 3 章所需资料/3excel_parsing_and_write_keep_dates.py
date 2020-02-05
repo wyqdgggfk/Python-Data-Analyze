6cb69abe0220bc06ead9281c004b5f796dc0063d
Jason Wang <wyqdgggfk@aol.com>
2020-02-06 00:15:14 +0800

    Update 3excel_parsing_and_write_keep_dates.py

--- 第 3 章所需资料/3excel_parsing_and_write_keep_dates.py
+++ 第 3 章所需资料/3excel_parsing_and_write_keep_dates.py
@@ -12,7 +12,7 @@ with open_workbook(input_file) as workbook:
 		row_list_output = [] # 实际上这个 row_list_output 根本不影响输出文件
 		for col_index in range(worksheet.ncols):
 			"""如果这个单元格是时间"""
-			if worksheet.cell_type(row_index, col_index) == 3:
+			if worksheet.cell_type(row_index, col_index) == 3: # 可查看 xlrd 的文档，检验单元格类型是否为数字 3，类型为 3 表示包含日期数据
 				date_cell = xldate_as_tuple(worksheet.cell_value(row_index, col_index), workbook.datemode)
 				date_cell = date(*date_cell[0:3]).strftime('%m/%d/%Y')
 				row_list_output.append(date_cell) # 实际上这个 row_list_output 根本不影响输出文件
