a8744a3b15fd0181a80b24724c940dac51eb226b
Jason Wang <wyqdgggfk@aol.com>
2020-02-06 00:40:07 +0800

    Update 3excel_parsing_and_write_keep_dates.py

--- 第 3 章所需资料/3excel_parsing_and_write_keep_dates.py
+++ 第 3 章所需资料/3excel_parsing_and_write_keep_dates.py
@@ -13,7 +13,7 @@ with open_workbook(input_file) as workbook:
 		for col_index in range(worksheet.ncols):
 			"""如果这个单元格是时间"""
 			if worksheet.cell_type(row_index, col_index) == 3: # 可查看 xlrd 的文档，检验单元格类型是否为数字 3，类型为 3 表示包含日期数据
-				date_cell = xldate_as_tuple(worksheet.cell_value(row_index, col_index), workbook.datemode)
+				date_cell = xldate_as_tuple(worksheet.cell_value(row_index, col_index), workbook.datemode) # 参数 workbook.datemode 是必需的，可以确定日期是基于 1900 年还是 1904 年
 				date_cell = date(*date_cell[0:3]).strftime('%m/%d/%Y')
 				row_list_output.append(date_cell) # 实际上这个 row_list_output 根本不影响输出文件
 				output_worksheet.write(row_index,col_index,date_cell)
