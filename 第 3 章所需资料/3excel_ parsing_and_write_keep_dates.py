088908dd10a157e631f2cfdceeb883654b3e3589
Jason Wang <wyqdgggfk@aol.com>
2020-02-05 21:13:43 +0800

    Update 3excel_ parsing_and_write_keep_dates.py

--- 第 3 章所需资料/3excel_ parsing_and_write_keep_dates.py
+++ 第 3 章所需资料/3excel_ parsing_and_write_keep_dates.py
@@ -1,2 +1,13 @@
 import sys
 from datetime import date
+from xlrd import open_workbook,xldate_as_tuple
+from xlwt import Workbook
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+output_workbook = Workbook()
+output_worksheet = output_workbook.add_sheet('jan_2013_output')
+with open_workbook(input_file) as workbook:
+	worksheet = workbook.sheet_by_name('january_2013')
+	for row_index in range(worksheet.nrows):
+		row_list_output = []
+		for col_index in range(worksheet.ncols):
