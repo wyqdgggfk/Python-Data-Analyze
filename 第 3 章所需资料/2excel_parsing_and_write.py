d35c6bfffd99dff9f1f455fced2cef2daff91d00
Jason Wang <wyqdgggfk@aol.com>
2020-02-05 15:50:57 +0800

    Update 2excel_parsing_and_write.py

--- 第 3 章所需资料/2excel_parsing_and_write.py
+++ 第 3 章所需资料/2excel_parsing_and_write.py
@@ -1,3 +1,8 @@
 import sys
 from xlrd import open_workbook
 from xlwt import Workbook
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+output_workbook = Workbook()
+output_worksheet = output_workbook.add_sheet('jan_2013_output')
+with open_workbook(input_file) as workbook:
