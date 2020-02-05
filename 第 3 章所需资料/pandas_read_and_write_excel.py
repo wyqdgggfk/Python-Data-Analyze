644e876030d6a809438fd3b2c2a2297716efd04a
Jason Wang <wyqdgggfk@aol.com>
2020-02-06 01:28:40 +0800

    update code

--- 第 3 章所需资料/pandas_read_and_write_excel.py
+++ 第 3 章所需资料/pandas_read_and_write_excel.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-data_frame = pd.read_excel(input_file,sheetname='january_2013')
+data_frame = pd.read_excel(input_file,sheet_name='january_2013')
 writer = pd.ExcelWriter(output_file)
 data_frame.to_excel(writer,sheet_name='jan_13_output',index=False)
 writer.save()
