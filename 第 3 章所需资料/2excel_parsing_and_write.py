90c0eeed9beb2cab611f71a433df6c58471e9766
Jason Wang <wyqdgggfk@aol.com>
2020-02-05 16:24:42 +0800

    Update 2excel_parsing_and_write.py

--- 第 3 章所需资料/2excel_parsing_and_write.py
+++ 第 3 章所需资料/2excel_parsing_and_write.py
@@ -3,7 +3,11 @@ from xlrd import open_workbook
 from xlwt import Workbook
 input_file = sys.argv[1]
 output_file = sys.argv[2]
-output_workbook = Workbook()
-output_worksheet = output_workbook.add_sheet('jan_2013_output')
+output_workbook = Workbook() # 初始化一个 output_workbook 对象
+output_worksheet = output_workbook.add_sheet('jan_2013_output') # 给 output_workbook 加入一个 sheet，取名 jan_2013_output
 with open_workbook(input_file) as workbook:
 	worksheet = Workbook.sheet_by_name('january_2013')
+	for row_index in range(worksheet.nrows):
+		for column_index in range(worksheet.ncols):
+			output_worksheet.write(row_index,column_index,worksheet.cell_value(row_index,column_index))
+output_workbook.save(output_file)
