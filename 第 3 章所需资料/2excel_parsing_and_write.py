8cf3bb843ab246efc6472bcf3ee7df0892e09087
Jason Wang <wyqdgggfk@aol.com>
2020-02-05 16:10:07 +0800

    Update 2excel_parsing_and_write.py

--- 第 3 章所需资料/2excel_parsing_and_write.py
+++ 第 3 章所需资料/2excel_parsing_and_write.py
@@ -6,3 +6,4 @@ output_file = sys.argv[2]
 output_workbook = Workbook()
 output_worksheet = output_workbook.add_sheet('jan_2013_output')
 with open_workbook(input_file) as workbook:
+	worksheet = Workbook.sheet_by_name('january_2013')
