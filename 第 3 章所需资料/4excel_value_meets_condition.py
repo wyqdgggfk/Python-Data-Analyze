181da55f273f9dedf4ed0abf1ac9d79b98a8e5c2
Jason Wang <wyqdgggfk@aol.com>
2020-02-06 17:29:50 +0800

    Update 4excel_value_meets_condition.py

--- 第 3 章所需资料/4excel_value_meets_condition.py
+++ 第 3 章所需资料/4excel_value_meets_condition.py
@@ -5,3 +5,7 @@ from xlwt import Workbook
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 output_workbook = Workbook()
+output_worksheet = output_workbook.add_sheet('jan_2013_output')
+sale_amount_column_index = 3
+with open_workbook(input_file) as workbook:
+	
