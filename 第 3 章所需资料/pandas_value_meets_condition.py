ecb6a4a251507143d6a4c2610ee660b93d73f0ad
Jason Wang <wyqdgggfk@aol.com>
2020-02-06 22:41:05 +0800

    Update pandas_value_meets_condition.py

--- 第 3 章所需资料/pandas_value_meets_condition.py
+++ 第 3 章所需资料/pandas_value_meets_condition.py
@@ -2,3 +2,6 @@ import pandas as pd
 import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
+data_frame = pd.read_excel(input_file,'january_2013',index_col=None)
+data_frame_value_meets_condition = data_frame[data_frame['Sale Amount'].astype(float) > 1400.0]
+writer = pd.ExcelWriter(output_file)
