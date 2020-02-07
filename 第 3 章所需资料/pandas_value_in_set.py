bd9eb320b749d635b6209918f22d7fed95633cd5
Jason Wang <wyqdgggfk@aol.com>
2020-02-07 16:21:08 +0800

    modified code

--- 第 3 章所需资料/pandas_value_in_set.py
+++ 第 3 章所需资料/pandas_value_in_set.py
@@ -3,7 +3,7 @@ import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 data_frame = pd.read_excel(input_file,'january_2013',index_col=None)
-important_dates = ['01/04/2013','01/31/2013']
+important_dates = ['01/24/2013','01/31/2013']
 data_frame_value_in_set = data_frame[data_frame['Purchase Date'].isin(important_dates)]
 writer = pd.ExcelWriter(output_file)
 data_frame_value_in_set.to_excel(writer,sheet_name='jan_13_output',index=False)
