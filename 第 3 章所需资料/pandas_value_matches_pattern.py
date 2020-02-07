4cef8dcaee3c63dd9347072e94d22cb9dcdcba1d
Jason Wang <wyqdgggfk@aol.com>
2020-02-07 21:05:10 +0800

    Update pandas_value_matches_pattern.py

--- 第 3 章所需资料/pandas_value_matches_pattern.py
+++ 第 3 章所需资料/pandas_value_matches_pattern.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sys
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+input_file = sys.argv[1] # 此处为 sales_2013.xlsx
+output_file = sys.argv[2] # 此处为 pandas_output
 data_frame = pd.read_excel(input_file,'january_2013',index_col=None)
 data_frame_value_matchs_pattern = data_frame[data_frame['Customer Name'].str.startswith("J")]
 writer = pd.ExcelWriter(output_file)
