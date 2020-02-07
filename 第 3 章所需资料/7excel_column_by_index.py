4bcb452ed8ce06e7d5bb0d352e4cb80df647406c
Jason Wang <wyqdgggfk@aol.com>
2020-02-07 22:17:28 +0800

    Update 7excel_column_by_index.py

--- 第 3 章所需资料/7excel_column_by_index.py
+++ 第 3 章所需资料/7excel_column_by_index.py
@@ -6,3 +6,6 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 output_workbook	= Workbook()
 output_worksheet = output_workbook.add_sheet('jan_2013_output')
+my_columns = [1,4]
+with open_workbook(input_file) as workbook:
+	
