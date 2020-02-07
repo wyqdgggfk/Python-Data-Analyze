f2440f7777538efbebb53cc02d27ef23e30df5b3
Jason Wang <wyqdgggfk@aol.com>
2020-02-07 20:32:19 +0800

    Update 6excel_value_matches_pattern.py

--- 第 3 章所需资料/6excel_value_matches_pattern.py
+++ 第 3 章所需资料/6excel_value_matches_pattern.py
@@ -3,3 +3,15 @@ import sys
 from datetime import date
 from xlrd import open_workbook,xldate_as_tuple
 from xlwt import Workbook
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+output_workbook = Workbook()
+output_worksheet = output_workbook.add_sheet('jan_2013_output')
+pattern = re.compile(r'(?P<my_pattern>^J.*)')
+customer_name_column_index = 1
+with open_workbook(input_file) as workbook:
+	worksheet = workbook.sheet_by_name('january_2013')
+	data = []
+	header = worksheet.row_values(0)
+	data.append(header)
+	
