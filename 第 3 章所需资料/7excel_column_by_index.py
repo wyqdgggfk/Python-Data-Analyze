73a9b86e64f39231f46731a07f0f9db14fff12df
Jason Wang <wyqdgggfk@aol.com>
2020-02-07 22:13:17 +0800

    Update 7excel_column_by_index.py

--- 第 3 章所需资料/7excel_column_by_index.py
+++ 第 3 章所需资料/7excel_column_by_index.py
@@ -2,3 +2,7 @@ import sys
 from datetime import date
 from xlrd import open_workbook,xldate_as_tuple
 from xlwt import Workbook
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+output_workbook	= Workbook()
+output_worksheet = output_workbook.add_sheet('jan_2013_output')
