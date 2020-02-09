bbfd1987b5a7c3456d5f37ec9ce6d5b6cb41f559
Jason Wang <wyqdgggfk@aol.com>
2020-02-09 19:37:28 +0800

    Update 8excel_column_by_name.py

--- 第 3 章所需资料/8excel_column_by_name.py
+++ 第 3 章所需资料/8excel_column_by_name.py
@@ -10,5 +10,12 @@ my_columns = ['Customer ID','Purchase Date']
 with open_workbook(input_file) as workbook:
 	worksheet = workbook.sheet_by_name('january_2013')
 	data = [my_columns]
-	header_list = worksheet.row_values(0)
+	header_list = worksheet.row_values(0) # 此处存储的是第一排的标题
+	header_index_list = []
 	
+	"""查找所需的索引值"""	
+	for header_index in range(len(header_list)):
+		if header_list[header_index] in my_columns:
+			header_index_list.append(header_index)
+	for row_index in range(1,worksheet.nrows):
+		row_list = []
