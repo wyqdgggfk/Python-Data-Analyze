1d54fb8cec2d7decb41e15e1a92f7d2253318132
Jason Wang <wyqdgggfk@aol.com>
2020-02-09 16:19:50 +0800

    Update pandas_column_by_index.py

--- 第 3 章所需资料/pandas_column_by_index.py
+++ 第 3 章所需资料/pandas_column_by_index.py
@@ -3,7 +3,7 @@ import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 data_frame = pd.read_excel(input_file,'january_2013',index_col=None)
-data_frame_column_by_index = data_frame.iloc[:[1,4]]
+data_frame_column_by_index = data_frame.iloc[:,[1,4]]
 writer = pd.ExcelWriter(output_file)
 data_frame_column_by_index.to_excel(writer,sheet_name='jan_13_output',index=False)
 writer.save()
