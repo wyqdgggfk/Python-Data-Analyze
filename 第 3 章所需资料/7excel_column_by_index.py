029c3b1522a059dea13ad77de3ab5f5873cea3d4
Jason Wang <wyqdgggfk@aol.com>
2020-02-09 15:15:14 +0800

    Update 7excel_column_by_index.py

--- 第 3 章所需资料/7excel_column_by_index.py
+++ 第 3 章所需资料/7excel_column_by_index.py
@@ -8,4 +8,9 @@ output_workbook	= Workbook()
 output_worksheet = output_workbook.add_sheet('jan_2013_output')
 my_columns = [1,4]
 with open_workbook(input_file) as workbook:
-	
+	worksheet = workbook.sheet_by_name('january_2013')
+	data = []
+	for row_index in range(worksheet.nrows):
+		row_list = []
+		for column_index in my_columns:
+			
