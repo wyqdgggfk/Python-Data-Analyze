57b16ab63e792aee6e9560f638d1c18c8f7f970d
Jason Wang <wyqdgggfk@aol.com>
2020-02-09 18:25:16 +0800

    Update 8excel_column_by_name.py

--- 第 3 章所需资料/8excel_column_by_name.py
+++ 第 3 章所需资料/8excel_column_by_name.py
@@ -7,3 +7,7 @@ output_file = sys.argv[2] # 此处为 8output.xls
 output_workbook = Workbook()
 output_worksheet = output_workbook.add_sheet('jan_2013_output')
 my_columns = ['Customer ID','Purchase Date']
+with open_workbook(input_file) as workbook:
+	worksheet = workbook.sheet_by_name('january_2013')
+	data = [my_columns]
+	
