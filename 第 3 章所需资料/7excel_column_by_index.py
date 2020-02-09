ec03d1a25d23c50c483880b857986e984679bdc9
Jason Wang <wyqdgggfk@aol.com>
2020-02-09 15:32:06 +0800

    Update 7excel_column_by_index.py

--- 第 3 章所需资料/7excel_column_by_index.py
+++ 第 3 章所需资料/7excel_column_by_index.py
@@ -13,4 +13,16 @@ with open_workbook(input_file) as workbook:
 	for row_index in range(worksheet.nrows):
 		row_list = []
 		for column_index in my_columns:
-			
+			cell_value = worksheet.cell_value(row_index, column_index)
+			cell_type = worksheet.cell_type(row_index, column_index)
+			if cell_type == 3:
+				date_cell = xldate_as_tuple(cell_value, workbook.datemode)
+				date_cell = date(*date_cell[0:3]).strftime('%m/%d/%Y')
+				row_list.append(date_cell)
+			else:
+				row_list.append(cell_value)
+		data.append(row_list)
+	for list_index,output_list in enumerate(data):
+		for element_index,element in enumerate(data):
+			output_worksheet.write(list_index,element_index,element)
+output_workbook.save(output_file)
