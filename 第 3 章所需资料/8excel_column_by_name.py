a8e3348983905a1f322a80f41192e17a26c8f912
Jason Wang <wyqdgggfk@aol.com>
2020-02-09 17:58:48 +0800

    Update 8excel_column_by_name.py

--- 第 3 章所需资料/8excel_column_by_name.py
+++ 第 3 章所需资料/8excel_column_by_name.py
@@ -4,4 +4,6 @@ from xlrd import open_workbook,xldate_as_tuple
 from xlwt import Workbook
 input_file = sys.argv[1] # 此处为 sales_2013.xlsx 
 output_file = sys.argv[2] # 此处为 8output.xls
-output_workboos = Workbook()
+output_workbook = Workbook()
+output_worksheet = output_workbook.add_sheet('jan_2013_output')
+my_columns = ['Customer ID','Purchase Date']
