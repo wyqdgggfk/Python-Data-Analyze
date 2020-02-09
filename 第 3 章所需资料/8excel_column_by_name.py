b217d340e703739541873ab21a450ea7e6edee95
Jason Wang <wyqdgggfk@aol.com>
2020-02-09 19:43:13 +0800

    Update 8excel_column_by_name.py

--- 第 3 章所需资料/8excel_column_by_name.py
+++ 第 3 章所需资料/8excel_column_by_name.py
@@ -19,3 +19,17 @@ with open_workbook(input_file) as workbook:
 			header_index_list.append(header_index)
 	for row_index in range(1,worksheet.nrows):
 		row_list = []
+		for column_index in header_index_list:
+			cell_value = worksheet.cell_value(row_index, column_index)
+			cell_type = worksheet.cell_type(row_index, column_index)
+			if cell_type == 3:
+				date_cell = xldate_as_tuple(cell_value, workbook.datemode)
+				date_cell = date(*date_cell[0:3]).strftime('%m/%d/%Y')
+				row_list.append(date_cell)
+			else:
+				row_list.append(cell_value)
+		data.append(row_list)
+	for list_index,output_list in enumerate(data):
+		for element_index,element in enumerate(output_list):
+			output_worksheet.write(list_index,element_index,element) # 按行列顺序写入数据
+output_workbook.save(output_file)
