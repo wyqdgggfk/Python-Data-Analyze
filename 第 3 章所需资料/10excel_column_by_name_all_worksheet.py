809d2d2de02510de4ae97a97b392d1111f45d189
Jason Wang <wyqdgggfk@aol.com>
2020-02-10 17:29:23 +0800

    Update 10excel_column_by_name_all_worksheet.py

--- 第 3 章所需资料/10excel_column_by_name_all_worksheet.py
+++ 第 3 章所需资料/10excel_column_by_name_all_worksheet.py
@@ -23,3 +23,14 @@ with open_workbook(input_file) as workbook:
 			for column_index in index_of_cols_to_keep:
 				cell_value = worksheet.cell_value(row_index,column_index)
 				cell_type = worksheet.cell_type(row_index,column_index)
+				if cell_type == 3:
+					date_cell = xldate_as_tuple(cell_value, workbook.datemode)
+					date_cell = date(*date_cell[0:3]).strftime('%m/%d/%Y')
+					row_list.append(date_cell)
+				else:
+					row_list.append(cell_value)
+			data.append(row_list)
+	for list_index,output_list in enumerate(data):
+		for element_index,element in enumerate(output_list):
+			output_worksheet.write(list_index,element_index,element)
+output_workbook.save(output_file)
