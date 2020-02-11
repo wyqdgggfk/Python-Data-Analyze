8502693177c2231223834eb1236cae6d81808f17
Jason Wang <wyqdgggfk@aol.com>
2020-02-11 22:37:19 +0800

    Update 11excel_value_meets_condition_set_of_worksheets.py

--- 第 3 章所需资料/11excel_value_meets_condition_set_of_worksheets.py
+++ 第 3 章所需资料/11excel_value_meets_condition_set_of_worksheets.py
@@ -7,4 +7,21 @@ output_file = sys.argv[2]
 output_workbook = Workbook()
 output_worksheet = output_workbook.add_sheet('set_of_worksheets')
 my_sheets = [0,1]
-threshold = 
+threshold = 1900.0
+sales_column_index = 3
+first_worksheet = True
+with open_workbook(input_file) as workbook:
+	data = []
+	for sheet_index in range(workbook.nsheets):
+		if sheet_index in my_sheets:
+			worksheet = workbook.sheet_by_index(sheet_index)
+			if first_worksheet:
+				header_row = worksheet.row_values(0)
+				data.append(header_row)
+				first_worksheet = False
+			for row_index in range(1,worksheet.nrows):
+				row_list = []
+				sales_amount = worksheet.cell_value(row_index, sales_column_index)
+				if sales_amount > threshold:
+					for column_index in range(worksheet.ncols):
+						cell_value = worksheet.cell_value(row_index, column_index)
