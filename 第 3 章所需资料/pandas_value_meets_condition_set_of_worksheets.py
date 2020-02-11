0c4f934afb3a72b1dfbc348d5af7beaeb9cbb164
Jason Wang <wyqdgggfk@aol.com>
2020-02-11 23:17:55 +0800

    Update pandas_value_meets_condition_set_of_worksheets.py

--- 第 3 章所需资料/pandas_value_meets_condition_set_of_worksheets.py
+++ 第 3 章所需资料/pandas_value_meets_condition_set_of_worksheets.py
@@ -8,5 +8,7 @@ data_frame = pd.read_excel(input_file,sheet_name=my_sheets,index_col=None)
 row_list = []
 for worksheet_name,data in data_frame.items():
 	row_list.append(data[data['Sale Amount'].astype(float) > threhold])
+filtered_rows = pd.concat(row_list,axis=0,ignore_index=True)
+writer = pd.ExcelWriter(output_file)
 filtered_rows.to_excel(writer,sheet_name='set_of_worksheets',index=False)
 writer.save()
