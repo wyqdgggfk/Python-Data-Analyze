d488c2f296ea44e418543f9a0219d80942b7aa82
Jason Wang <wyqdgggfk@aol.com>
2020-02-14 11:53:29 +0800

    Update 12excel_introspect_all_workbooks.py

--- 第 3 章所需资料/12excel_introspect_all_workbooks.py
+++ 第 3 章所需资料/12excel_introspect_all_workbooks.py
@@ -6,6 +6,10 @@ input_directory = sys.argv[1]
 workbook_counter = 0
 for input_file in glob.glob(os.path.join(input_directory, '*.xls')): # 原书中的匹配是 '*.xls*',但这会匹配 xlsx 的文件
 	workbook = open_workbook(input_file)
+	"""需要注意下面的这种 print 方式,适用于单次 print
+		如果您的代码在编写的过程中有多次修改,或者本身就有多次 print
+		我一般会建议您创建一个 message 变量,把要 print 的内容赋值给 message 变量
+		然后 print(message)"""
 	print('Workbook: %s' % os.path.basename(input_file))
 	print('Number of worksheets:%d' % workbook.nsheets)
 	for worksheet in workbook.sheets():
