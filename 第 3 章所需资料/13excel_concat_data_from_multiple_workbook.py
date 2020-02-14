65df178179fa717369827930bde9c10529e7b72d
Jason Wang <wyqdgggfk@aol.com>
2020-02-14 12:59:03 +0800

    Update 13excel_concat_data_from_multiple_workbook.py

--- 第 3 章所需资料/13excel_concat_data_from_multiple_workbook.py
+++ 第 3 章所需资料/13excel_concat_data_from_multiple_workbook.py
@@ -10,3 +10,4 @@ output_workbook = Workbook()
 output_worksheet = output_workbook.add_sheet('all_data_all_workbooks')
 data = []
 first_worksheet = True
+for input_file in glob.glob(os.path.join(input_folder, '*.xls')) # 原书中此处匹配的是 '*.xls*'
