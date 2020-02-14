5acf24801f9a357fbaf4329cef492a116d5e243c
Jason Wang <wyqdgggfk@aol.com>
2020-02-14 22:20:46 +0800

    Update 4db_mysql_load_from_csv.py

--- 第 4 章所需资料/4db_mysql_load_from_csv.py
+++ 第 4 章所需资料/4db_mysql_load_from_csv.py
@@ -5,3 +5,8 @@ from datetime import datetime,date
 
 # CSV 输入文件的路径和文件名
 input_file = sys.argv[1]
+con = MySQLdb.connect(host='localhost',port=3306,db='my_suppliers',user='root',passwd='JKWing1990')
+c = con.cursor()
+# 向 Suppliers 表中插入数据
+file_reader = csv.reader(open(input_file,'r',newline=''))
+header = next(file_reader)
